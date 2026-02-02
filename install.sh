53da055e72527e4ed9e11b3ee8afc01a3f2c1f91
ujjwal-hackerscolony <ut797914@gmail.com>
2026-02-02 19:01:14 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
-h
+#!/data/data/com.termux/files/usr/bin/bash
+
+set -e
+
+clear
+echo "================================="
+echo " Kali NetHunter Rootless Installer"
+echo " Author: Master_Termux"
+echo "================================="
+sleep 2
